fde7addb167b6f5ff40fa506da36b56e03ba68a2
Karl Kirsipuu <karl_kirsipuu@hot.ee>
2017-11-13 14:21:58 +0200

    add multiplication table

---
+++ Tabel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tabel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 0; i <= 6; i++)
+            {
+                Console.Write(i + "\t");
+                for (int j = 1; j <= 6; j++)
+                {
+                    if (i > 0) Console.Write(i * j + "\t");
+                    else Console.Write(j + "\t");
+                }
+                Console.Write("\n");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
